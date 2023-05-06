0198e519c1d91b436eaa77fcab32041d963722d4
Giloushaker <gilouredstoneomg@gmail.com>
2023-05-06 14:36:19 +0200

    add titleright right align

--- components/TitleBar.vue
+++ components/TitleBar.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="titlebar">
-    <div class="titlebar-content" id="titlebar-content">
+    <div class="titlebar-content titlebar-left inline" id="titlebar-content">
       <h1>NR-Editor</h1>
+      <slot />
     </div>
-    <slot />
+    <div
+      class="titlebar-content titlebar-right inline"
+      id="titlebar-content-right"
+    >
+      <div>Right1</div>
+      <div>Right2</div>
+    </div>
   </div>
 </template>
 
 <style>
 .titlebar {
+  display: flex;
   position: sticky;
   top: 0;
   left: 0;
@@ -20,8 +28,17 @@
   box-sizing: border-box;
   z-index: 1;
 }
-.titlebar-content {
+.titlebar-left {
   display: flex;
+  align-items: center;
+  height: 100%;
+  padding: 0 10px;
+}
+.titlebar-right {
+  margin-left: auto;
+  display: flex;
+  flex-flow: row-reverse;
+  flex-direction: row-reverse;
   align-items: center;
   height: 100%;
   padding: 0 10px;
