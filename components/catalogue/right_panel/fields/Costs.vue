e5607b93e84afc269d276b339eef1ab3aaa1d11f
Vincent Massias <vi.massias@gmail.com>
2023-05-13 08:27:24 +0200

    fixed costs use all costs in catalogue cost types

--- components/catalogue/right_panel/fields/Costs.vue
+++ components/catalogue/right_panel/fields/Costs.vue
@@ -2,29 +2,68 @@
   <fieldset>
     <legend>Costs</legend>
     <div class="costs">
-      <div v-for="cost of item.costs">
+      <div v-for="cost of catalogue.costTypes">
         <label>{{ cost.name }}: </label>
-        <UtilNumberInput v-model="cost.value" class="input" />
+        <UtilNumberInput
+          v-if="selectedCosts[cost.id]"
+          v-model="selectedCosts[cost.id].value"
+          class="input"
+          @change="changed"
+        />
       </div>
     </div>
   </fieldset>
 </template>
 
 <script lang="ts">
+import { Catalogue } from "~/assets/shared/battlescribe/bs_main_catalogue";
 import { BSICost } from "~/assets/shared/battlescribe/bs_types";
 
 export default {
   emits: ["catalogueChanged"],
+  data() {
+    return {
+      selectedCosts: {} as Record<string, BSICost>,
+    };
+  },
+
+  created() {
+    this.update();
+  },
+
   props: {
     item: {
       type: Object as PropType<{ costs: BSICost[] }>,
+      required: true,
+    },
+
+    catalogue: {
+      type: Object as PropType<Catalogue>,
       required: true,
     },
   },
 
   methods: {
     changed() {
+      this.item.costs = Object.values(this.selectedCosts);
       this.$emit("catalogueChanged");
+    },
+
+    update() {
+      this.selectedCosts = {};
+      for (let cost of this.item.costs) {
+        this.selectedCosts[cost.typeId] = {
+          name: cost.name,
+          typeId: cost.typeId,
+          value: cost.value,
+        };
+      }
+    },
+  },
+
+  watch: {
+    item() {
+      this.update();
     },
   },
 };
