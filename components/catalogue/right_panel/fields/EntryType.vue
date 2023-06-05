8426b9c2214f7a4d9d13615c82d5491869d44d98
Vincent Massias <vi.massias@gmail.com>
2023-06-05 10:53:24 +0200

    added missing changed event

--- components/catalogue/right_panel/fields/EntryType.vue
+++ components/catalogue/right_panel/fields/EntryType.vue
@@ -5,7 +5,7 @@
       <tr>
         <td>Type:</td>
         <td>
-          <select v-model="item.type">
+          <select v-model="item.type" @change="changed">
             <option value="model">Model</option>
             <option value="upgrade">Upgrade</option>
             <option value="unit">Unit</option>
