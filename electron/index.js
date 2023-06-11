a9d935cb14db9295829c3b9957ee3b1baa18f20c
Giloushaker <gilouredstoneomg@gmail.com>
2023-06-11 14:02:22 +0200

    fix links not working

--- electron/index.js
+++ electron/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, protocol, session } = require("electron");
+const { app, BrowserWindow, ipcMain, protocol, session, shell } = require("electron");
 const path = require("path");
 const { autoUpdater } = require("electron-updater");
 const dialog = require("electron").dialog;
