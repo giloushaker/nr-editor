d7db82a90b740d82a9c39455d4fa10bc79660ead
giloushaker <55761672+giloushaker@users.noreply.github.com>
2023-06-15 12:11:30 +0200

    Add files via upload

---
+++ _nuxt/_...slugs_.fc67704d.js
@@ -0,0 +1,1 @@
+import{a as s,t}from"./entry.02aa96f5.js";const _={};function a(r,e){return t(r.$route.params)}const o=s(_,[["render",a]]);export{o as default};
---
+++ _nuxt/_id_.575d7c28.js
@@ -0,0 +1,1 @@
+import{a as t,t as a}from"./entry.02aa96f5.js";const e={};function s(r,n){return a(r.$route.params)}const _=t(e,[["render",s]]);export{_ as default};
