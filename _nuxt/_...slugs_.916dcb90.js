30aa1ed62d8a577cfae8506218d66faf021023ad
giloushaker <giloushaker@users.noreply.github.com>
2023-06-20 20:57:36 +0000

    deploy: 635c7769d09ea63323715c2e71ff727cf8db58db

--- _nuxt/_...slugs_.916dcb90.js
+++
@@ -1,2 +0,0 @@
-import{a as s,t}from"./entry.a07e3c21.js";const _={};function a(r,e){return t(r.$route.params)}const o=s(_,[["render",a]]);export{o as default};
-//# sourceMappingURL=_...slugs_.916dcb90.js.map
