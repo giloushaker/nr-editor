310e6caaa344c008b81d042c2e365f290cb5c2a8
giloushaker <giloushaker@users.noreply.github.com>
2023-06-24 20:58:06 +0000

    deploy: 635c7769d09ea63323715c2e71ff727cf8db58db

--- _nuxt/_...slugs_.5d9278ab.js
+++
@@ -1,2 +0,0 @@
-import{a as s,t}from"./entry.cdcbc1fc.js";const _={};function a(r,e){return t(r.$route.params)}const o=s(_,[["render",a]]);export{o as default};
-//# sourceMappingURL=_...slugs_.5d9278ab.js.map
--- _nuxt/_id_.645a577a.js
+++
@@ -1,2 +0,0 @@
-import{a as t,t as a}from"./entry.cdcbc1fc.js";const e={};function s(r,n){return a(r.$route.params)}const _=t(e,[["render",s]]);export{_ as default};
-//# sourceMappingURL=_id_.645a577a.js.map
