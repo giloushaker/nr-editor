a87dcccd7b50b41529fa2182ca0acd16f3b05a72
giloushaker <giloushaker@users.noreply.github.com>
2024-03-15 20:26:09 +0000

    deploy: 336ca13bda424f4af05d0eb5b0d69f2b1c82a2a7

--- _nuxt/_id_.0809e041.js
+++ _nuxt/_id_.0809e041.js
@@ -1,4 +1,4 @@
-import{u as e}from"./editorStore.a368e238.js";import{e as t,a as s,b as o,o as r,i as n}from"./entry.39976045.js";const a=t({setup(){return{store:e()}},data(){return{}},computed:{async system(){return await this.store.get_or_load_system(this.$route.params.id)}},methods:{}}),i={class:"ml-20px mt-10px h-full overflow-y-auto"},c=n("pre",null,`This tab is used to run scripts:
+import{u as e}from"./editorStore.5c906c74.js";import{e as t,a as s,b as o,o as r,i as n}from"./entry.bd384ed6.js";const a=t({setup(){return{store:e()}},data(){return{}},computed:{async system(){return await this.store.get_or_load_system(this.$route.params.id)}},methods:{}}),i={class:"ml-20px mt-10px h-full overflow-y-auto"},c=n("pre",null,`This tab is used to run scripts:
 Scripts will be able to export certain methods:
 
 // Returns the entries that the script should select, allowing the user to preview what would be changed by modify()
@@ -21,4 +21,4 @@ check(catalogues, entries)
 // export an object as \`docs\` with the key:value pairs corresponding to a function (such as \`select\`, \`modify\`,
 \`check\`)
         `,-1),d=[c];function u(p,h,l,m,f,_){return r(),o("div",i,d)}const b=s(a,[["render",u]]);export{b as default};
-//# sourceMappingURL=_id_.cd494beb.js.map
+//# sourceMappingURL=_id_.0809e041.js.map
