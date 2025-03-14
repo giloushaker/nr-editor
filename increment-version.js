2720d624cc065cd04155c5544080d95ea002189b
giloushaker <55761672+giloushaker@users.noreply.github.com>
2025-03-14 18:48:37 +0100

    update package-lock aswell

--- increment-version.js
+++ increment-version.js
@@ -1,19 +1,33 @@
 const fs = require("fs");
 
-const packagePath = "./package.json"
-const json = JSON.parse(fs.readFileSync(packagePath))
-let [_, major, minor, patch] = json.version.match(/([0-9]+)[.]([0-9]+)[.]([0-9]+)/)
-if (process.argv.includes("--major")) {
-    major = (Number(major) || 0) + 1
-    minor = 0;
-    patch = 0;
+function incrementObjectVersion(json) {
+    let [_, major, minor, patch] = json.version.match(/([0-9]+)[.]([0-9]+)[.]([0-9]+)/);
+    if (process.argv.includes("--major")) {
+        major = (Number(major) || 0) + 1;
+        minor = 0;
+        patch = 0;
+    }
+    else if (process.argv.includes("--minor")) {
+        minor = (Number(minor) || 0) + 1;
+        patch = 0;
+    }
+    else {
+        patch = (Number(patch) || 0) + 1;
+    }
+    json.version = `${major}.${minor}.${patch}`;
+    return json.version
 }
-else if (process.argv.includes("--minor")) {
-    minor = (Number(minor) || 0) + 1
-    patch = 0;
+
+function incrementJsonFileVersion(path) {
+    const json = JSON.parse(fs.readFileSync(path));
+    const version = incrementObjectVersion(json);
+    fs.writeFileSync(path, JSON.stringify(json, null, 2));
+    return version
 }
-else {
-    patch = (Number(patch) || 0) + 1
+function setJsonFileVersion(path, version) {
+    const json = JSON.parse(fs.readFileSync(path));
+    json.version = version
+    fs.writeFileSync(path, JSON.stringify(json, null, 2));
 }
-json.version = `${major}.${minor}.${patch}`
-fs.writeFileSync(packagePath, JSON.stringify(json, null, 2));
+const newVersion = incrementJsonFileVersion("./package.json");
+setJsonFileVersion("./package-lock.json", newVersion);
