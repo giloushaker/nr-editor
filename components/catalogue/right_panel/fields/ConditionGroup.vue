054c19c50a9ca4022cff2d9b0eb1803f95dadbc9
giloushaker <55761672+giloushaker@users.noreply.github.com>
2025-11-19 16:53:19 +0100

    add Count and Not condition groups

--- components/catalogue/right_panel/fields/ConditionGroup.vue
+++ components/catalogue/right_panel/fields/ConditionGroup.vue
@@ -6,12 +6,28 @@
       <tr>
         <td>Type</td>
         <td>
-          <select v-model="item.type">
+          <select v-model="item.type" @change="onChange">
             <option value="or">Or</option>
             <option value="and">And</option>
+            <option value="not">Not</option>
+            <option value="count">Count</option>
           </select>
         </td>
       </tr>
+      <template v-if="item.type === 'count'">
+        <tr>
+          <td>Min</td>
+          <td>
+            <input type="number" v-model="item.min" />
+          </td>
+        </tr>
+        <tr>
+          <td>Max</td>
+          <td>
+            <input type="number" v-model="item.max" />
+          </td>
+        </tr>
+      </template>
     </table>
   </fieldset>
 </template>
@@ -25,6 +41,14 @@ export default {
     item: {
       type: Object as PropType<BSIConditionGroup & EditorBase>,
       required: true,
+    },
+  },
+  methods: {
+    onChange() {
+      if (this.item.type !== "count") {
+        delete this.item.min;
+        delete this.item.max;
+      }
     },
   },
 };
